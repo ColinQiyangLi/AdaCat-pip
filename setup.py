9429a8cdecb1dfd570a0acd46abfca580caa448a
Qiyang Li <colin.qiyang.li@gmail.com>
2022-06-15 20:45:13 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name = 'adacat',
+    packages = ['adacat'],
+    version = '0.1',
+    description = 'Adaptive categorical distribution',
+    author='Qiyang Li',
+    author_email='colin.qiyang.li@gmail.com',
+    url='https://github.com/ColinQiyangLi/AdaCat-pip',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
+)
