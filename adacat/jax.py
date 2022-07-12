90e93686a6de3048e44d94743153b02c371b2837
Qiyang Li <colin.qiyang.li@gmail.com>
2022-07-12 11:16:50 -0700

    v0.4 release

--- adacat/jax.py
+++ adacat/jax.py
@@ -39,26 +39,16 @@ class Adacat(distrax.Distribution):
         self.x_anchors = jnp.concatenate((jnp.zeros_like(self.x_cum[..., 0:1]), self.x_cum[..., :-1], jnp.ones_like(self.x_cum[..., 0:1]),), axis=-1)
         self.x_mids = (self.x_anchors[..., 1:] + self.x_anchors[..., :-1]) / 2.
 
+        self.prefix_first_moment = jnp.cumsum(self.x_mids * self.y_sizes, axis=-1)
+
         self.smooth_coeff = smooth_coeff
         self.eps = eps
 
     def _sample_n(self, key, n):
         batch_shape = self.h_logits.shape[:-1]
         length = len(batch_shape)
-        cat_dist = Categorical(logits=self.h_logits)
-
-        key_cat, key_unif = jax.random.split(key, 2)
-        indices = cat_dist.sample(seed=key_cat, sample_shape=(n,))
-
-        # shape [ x x x y ] => [y x x x ]
-        x_cum_t = self.x_cum.transpose(length, *range(length))
-        x_sizes_t = self.x_sizes.transpose(length, *range(length))
-
-        x_right = jnp.take_along_axis(x_cum_t, indices, axis=0)
-        x_size = jnp.take_along_axis(x_sizes_t, indices, axis=0)
-
-        x = x_right - x_size * jax.random.uniform(key_unif, shape=x_size.shape)
-        return jnp.clip(x, 0., 1.)  # make sure everything is fine
+        y = jax.random.uniform(key, (n,) + batch_shape)
+        return self.icdf(y)
 
     def smooth_log_prob(self, value):
         value = value[..., None]        
@@ -90,12 +80,89 @@ class Adacat(distrax.Distribution):
 
         return log_prob
 
+    def cdf(self, value):
+        batch_shape = self.x_cum.shape[:-1]
+        batch_numel = prod(batch_shape)
+        length = len(batch_shape)
+        
+        if length != 0:
+            value = jnp.broadcast_to(value, value.shape[:-length] + batch_shape)
+
+        value_fl = jnp.transpose(value.reshape((-1, batch_numel)), (1, 0))
+        x_cum_fl = self.x_cum.reshape((batch_numel, -1))
+
+        indices = jnp.clip(jax.vmap(jnp.searchsorted, in_axes=(0, 0), out_axes=0)(x_cum_fl, value_fl), 0, self.k - 1)
+
+        x_cum = jnp.transpose(jnp.take_along_axis(self.x_cum.reshape(-1, self.k), indices, axis=-1), (1, 0)).reshape(value.shape)
+        y_cum = jnp.transpose(jnp.take_along_axis(self.y_cum.reshape(-1, self.k), indices, axis=-1), (1, 0)).reshape(value.shape) 
+        x_sizes = jnp.transpose(jnp.take_along_axis(self.x_sizes.reshape(-1, self.k), indices, axis=-1), (1, 0)).reshape(value.shape) 
+        y_sizes = jnp.transpose(jnp.take_along_axis(self.y_sizes.reshape(-1, self.k), indices, axis=-1), (1, 0)).reshape(value.shape) 
+        
+        return (y_cum - (x_cum - value) / x_sizes * y_sizes)
+
+    def icdf(self, value):
+        batch_shape = self.x_cum.shape[:-1]
+        batch_numel = prod(batch_shape)
+        length = len(batch_shape)
+        
+        if length != 0:
+            value = jnp.broadcast_to(value, value.shape[:-length] + batch_shape)
+
+        value_fl = jnp.transpose(value.reshape((-1, batch_numel)), (1, 0))
+        y_cum_fl = self.y_cum.reshape((batch_numel, -1))
+
+        indices = jnp.clip(jax.vmap(jnp.searchsorted, in_axes=(0, 0), out_axes=0)(y_cum_fl, value_fl), 0, self.k - 1)
+
+        x_cum = jnp.transpose(jnp.take_along_axis(self.x_cum.reshape(-1, self.k), indices, axis=-1), (1, 0)).reshape(value.shape) 
+        y_cum = jnp.transpose(jnp.take_along_axis(self.y_cum.reshape(-1, self.k), indices, axis=-1), (1, 0)).reshape(value.shape)
+        x_sizes = jnp.transpose(jnp.take_along_axis(self.x_sizes.reshape(-1, self.k), indices, axis=-1), (1, 0)).reshape(value.shape) 
+        y_sizes = jnp.transpose(jnp.take_along_axis(self.y_sizes.reshape(-1, self.k), indices, axis=-1), (1, 0)).reshape(value.shape)
+
+        return (x_cum - (y_cum - value) / y_sizes * x_sizes)
+
     def prob(self, *args, **kwargs):
         return jnp.exp(self.log_prob(*args, **kwargs))
 
     def mean(self):
         return (self.x_mids * self.y_sizes).sum(axis=-1)
 
+    def median(self):
+        return self.icdf(jnp.ones_like(self.x_mids[..., 0]) * 0.5)
+
+    def robust_mean(self, lower_quantile, upper_quantile):
+        batch_shape = self.x_cum.shape[:-1]
+        batch_numel = prod(batch_shape)
+        length = len(batch_shape)
+        
+        if length != 0:
+            lower_quantile = jnp.broadcast_to(lower_quantile, lower_quantile.shape[:-length] + batch_shape)
+            upper_quantile = jnp.broadcast_to(upper_quantile, upper_quantile.shape[:-length] + batch_shape)
+
+        lower_value = self.icdf(lower_quantile)
+        upper_value = self.icdf(upper_quantile)
+        value_shape = lower_quantile.shape
+
+        y_cum_fl = self.y_cum.reshape((batch_numel, -1))
+        
+        lower_value_fl = jnp.transpose(lower_quantile.reshape((-1, batch_numel)), (1, 0))
+        lower_indices = jnp.clip(jax.vmap(jnp.searchsorted, in_axes=(0, 0), out_axes=0)(y_cum_fl, lower_value_fl), 0, self.k - 1)
+        upper_value_fl = jnp.transpose(upper_quantile.reshape((-1, batch_numel)), (1, 0))
+        upper_indices = jnp.clip(jax.vmap(jnp.searchsorted, in_axes=(0, 0), out_axes=0)(y_cum_fl, upper_value_fl), 0, self.k - 1)
+
+        lower_prefix = jnp.transpose(jnp.take_along_axis(self.prefix_first_moment.reshape(-1, self.k), lower_indices, axis=-1), (1, 0)).reshape(value_shape) 
+        upper_prefix = jnp.transpose(jnp.take_along_axis(self.prefix_first_moment.reshape(-1, self.k), upper_indices, axis=-1), (1, 0)).reshape(value_shape) 
+
+        lower_y_cum = jnp.transpose(jnp.take_along_axis(self.y_cum.reshape(-1, self.k), lower_indices, axis=-1), (1, 0)).reshape(value_shape) 
+        upper_y_cum = jnp.transpose(jnp.take_along_axis(self.y_cum.reshape(-1, self.k), upper_indices, axis=-1), (1, 0)).reshape(value_shape) 
+        lower_x_cum = jnp.transpose(jnp.take_along_axis(self.x_cum.reshape(-1, self.k), lower_indices, axis=-1), (1, 0)).reshape(value_shape) 
+        upper_x_cum = jnp.transpose(jnp.take_along_axis(self.x_cum.reshape(-1, self.k), upper_indices, axis=-1), (1, 0)).reshape(value_shape) 
+
+        fm = upper_prefix - lower_prefix \
+            + (lower_y_cum - lower_quantile) * (lower_value + lower_x_cum) / 2. \
+            - (upper_y_cum - upper_quantile) * (upper_value + upper_x_cum) / 2.
+
+        return fm / (upper_quantile - lower_quantile)
+        
     def entropy(self):
         return -(self.log_ratio * self.y_sizes).sum(axis=-1)
 
--- tests/test_func_jax.py
+++ tests/test_func_jax.py
@@ -3,6 +3,15 @@ import jax.numpy as jnp
 
 from adacat.jax import Adacat
 
+def test_cdf_icdf_consistency(key, k, batch_shape, sample_shape):
+    params = jax.random.normal(key, (*batch_shape, k * 2))
+    d = Adacat(params[..., :k], params[..., k:])
+
+    x = jax.random.uniform(key, sample_shape + batch_shape)
+    x_rec = d.icdf(d.cdf(x))
+
+    assert jnp.allclose(x, x_rec, atol=1e-03), jnp.max(jnp.abs((x - x_rec)))
+
 def test_mean(key, k, batch_shape):
     params = jax.random.normal(key, (*batch_shape, k * 2))
     d = Adacat(params[..., :k], params[..., k:])
@@ -25,7 +34,17 @@ def test_entropy(key, k, batch_shape):
 
     assert jnp.allclose(emp_ent, d.entropy(), atol=1e-02)
 
-key = jax.random.PRNGKey(0)
+key = jax.random.PRNGKey(135)
+
+test = test_cdf_icdf_consistency
+
+for test in [test_cdf_icdf_consistency]:
+    test(key, 10, (), ())
+    test(key, 10, (5,), ())
+    test(key, 10, (), (8,))
+    test(key, 10, (5,), (8,))
+    test(key, 10, (5, 2), (8,))
+    test(key, 10, (5, 2), (8, 5, 3, 2))
 
 test_mean(key, 10, ())
 test_entropy(key, 10, ())
